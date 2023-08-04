56f549493e4cee004547bccc9c68af133b7eed6b
dhkim <kdh4501@hicare.net>
2023-08-04 15:16:56 +0900

    feat WatchlistAdapter에 로그 삭제

--- app/src/main/java/com/dhkim/tvshows/adapters/WatchlistAdapter.kt
+++ app/src/main/java/com/dhkim/tvshows/adapters/WatchlistAdapter.kt
@@ -16,7 +16,6 @@ class WatchlistAdapter(
 ): RecyclerView.Adapter<WatchlistAdapter.TVShowViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TVShowViewHolder {
-        println("1111111  ${tvShows.size}")
         val binding: ItemContainerTvShowBinding = DataBindingUtil.inflate(
             LayoutInflater.from(parent.context),
             R.layout.item_container_tv_show,
@@ -28,12 +27,10 @@ class WatchlistAdapter(
     }
 
     override fun getItemCount(): Int {
-        println("22222  ${tvShows}")
         return tvShows.size
     }
 
     override fun onBindViewHolder(holder: TVShowViewHolder, position: Int) {
-        println("3333333  ${tvShows}")
         holder.bindTVShow(tvShows[position], watchlistListener)
     }
 
